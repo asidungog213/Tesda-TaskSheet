4d4a64a2d5af5bdc31962bce9c75738fe159ee7f
asidungog213 <realestasi@gmail.com>
2024-08-24 22:40:28 +0800

    Add files via upload

---
+++ Task115.java
@@ -0,0 +1,16 @@
+public class Task115 {
+    public static void main(String[] args) {
+        String a = "Wowerz";
+        String b = "Wow";
+        String c = a;
+        String d = c + "!";
+
+        boolean b1 = a.equals(b);
+        boolean b2 = d.equals(b + "!");
+        boolean b3 = !c.equals("Not Wow");
+
+        if (b1 && b2 && b3) {
+            System.out.println("Success!");
+        }
+    }
+}
