ac3819bae695c277e9ea0b277082592123c4b8bb
asidungog213 <realestasi@gmail.com>
2024-08-24 22:41:43 +0800

    Add files via upload

---
+++ H3110w0r1d.java
@@ -0,0 +1,12 @@
+public class H3110w0r1d {
+    public static void main(String[] args) {
+        byte b = 0;
+        short s = 1;
+        float f = 2.0f;
+        char c = 'H';
+        boolean bool = true;
+
+        String output = c + "3" + s + s + "0 w" + b + "r" + s + "d " + f + " " + bool;
+        System.out.println(output);
+    }
+}
